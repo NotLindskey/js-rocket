8e74af729fa4a9ea1d312a3620bfa4f6a875e09a
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-25 03:16:59 -0500

    random generated numbers to change state

--- js/rocket.js
+++ js/rocket.js
@@ -4,17 +4,32 @@ var countDownNumber = 10;
 var changeState = function (state) {
   document.body.className = 'body-state' + state;
   clearInterval(timer);
+  countDownNumber = 10;
+  document.getElementById('countdown').innerHTML = countDownNumber;
 
+  // countdown
   if (state == 2) {
     timer = setInterval(function () {
-      document.getElementById('countdown').innerHTML = countDownNumber;
       countDownNumber = countDownNumber - 1;
+      document.getElementById('countdown').innerHTML = countDownNumber;
 
       if (countDownNumber <= 0) {
         changeState(3);
       }
     }, 500);
   } else if (state == 3) {
+    var success = setTimeout(function () {
+      var randomNumber = Math.round(Math.random() * 10);
+
+      console.log('randomNumber: ', randomNumber);
+
+      // success
+      if (randomNumber > 5) {
+        changeState(4);
+      } else {
+        changeState(5); // oh no!
+      }
+    }, 2000);
   }
 };
 
