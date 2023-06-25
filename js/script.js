4e4f4d1a0ae4457e42825f9beb1a517a7ef04bc3
Linssi Colin Ruiz <linssi101@gmail.com>
2023-06-25 04:48:23 -0500

    setTimeout to redirect user after 10sec idle, removed code comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// redirect you to the Launch page after 10sec idle
+setTimeout(function () {
+  location.href = 'rocket.html';
+}, 10000);
+
 // this function does this.
 var doCoolStuff = function () {
   var currentClassName = document.getElementById('cool').className;
@@ -9,32 +14,3 @@ var doCoolStuff = function () {
     document.getElementById('cool').className = 'cool';
   }
 };
-
-var sayMyName = function (name) {
-  alert('my Name is: ' + name);
-};
-
-var car = {
-  make: 'VW',
-  type: 'polo',
-  color: 'blue',
-  isTurnedOn: false,
-  numberOfWheels: 4,
-  seats: ['seat 1', 'seat 2', 'seat 3', 'seat 4'],
-  turnOn: function () {
-    this.isTurnedOn = true;
-  },
-  fly: function () {
-    alert('fly');
-  },
-  switchCar: function (isOn) {
-    console.log('turn car ' + isOn);
-    if (isOn == true) {
-      this.isTurnedOn = true;
-    } else {
-      this.isTurnedOn = false;
-    }
-  },
-};
-
-console.log('hello!');
